de6cd030d90139ff3e72ef1d7c1886b60522e89b
Douglas Faria <hfsdouglas@gmail.com>
2024-10-09 18:47:27 -0300

    feat: add styles to form inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { Play } from "phosphor-react";
 
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home() {
     return (
@@ -8,10 +8,29 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input type="text" id="task"/>
+                    <TaskInput 
+                        type="text" 
+                        id="task" 
+                        placeholder="Dê um nome para o seu projeto"
+                        list="task-suggestions" 
+                    />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Projeto 4" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" id="minutesAmount" />
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60} 
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -24,10 +43,10 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <button type="submit">
+                <StartCountdownButton type="submit">
                     <Play size={24} />
                     Começar
-                </button>
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     )
